fafd2a605a033e9db3ece85b49d1707ef921a823
Bruno Câmara <110070619+bruno-bcab@users.noreply.github.com>
2022-10-23 18:44:49 -0300

    Add files via upload

---
+++ EXERCÍCIOS OPERAÇÕES BÁSICAS COM R E OPERADORES LÓGICOS.R
@@ -0,0 +1,21 @@
+DATARSU[ ,4]
+DATARSU[2, ]
+DATARSU[1:184, 1:5]
+DATARSU[, 'Iegm']
+DATARSU[70, 'Populacao']
+DATARSU['$Iegm', ]
+DATARSU[ , '$Partidos']
+DATARSU['$Partidos', ]
+DATARSU$Influencia_grupos_interesse
+DATARSU$Influencia_grupos_interesse[1]
+DATARSU[[5]][5]
+DATARSU[ ,2]
+DATARSU$Municipio <- as.factor(DATARSU$Municipio)
+Witch(DATARSU$Populacao.Length <= 2)
+install.packages("withr")
+pacman::p_data(which(x, arr.ind = FALSE, userNames = TRUE)
+library(Which)
+match(DATARSU$Partidos, DATARSU$Distancia_Aterro)               
+match(DATARSU$Municipio,DATARSU$Iegm)
+match(DATARSU$Influencia_grupos_interesse,DATARSU$Populacao)
+match(DATARSU$Iegm, DATARSU$Destinacao_final)
