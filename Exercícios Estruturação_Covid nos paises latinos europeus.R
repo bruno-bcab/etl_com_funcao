ca433d8ec03f9c945176e331cb7ac811976894da
Bruno Câmara <110070619+bruno-bcab@users.noreply.github.com>
2022-10-24 22:20:40 -0300

    Add files via upload

---
+++ Exercícios Estruturação_Covid nos paises latinos europeus.R
@@ -0,0 +1,75 @@
+library(data.table)
+library(dplyr)
+library(tidyverse)
+
+general_data<-fread("https://covid.ourworldindata.org/data/owid-covid-data.csv") # carrega dados de covid19 no mundo
+
+latin_america_countries <- c("Argentina", "Brazil", "Bolivia", "Chile", "Colombia", "Costa Rica", "Cuba", "Dominican Republic", "Ecuador", "El Salvador", "Guatemala", "Haiti", "Honduras", "Mexico", "Nicaragua", "Panama", "Paraguay", "Peru", "Uruguay", "Venezuela") # vetor que identifica países latino americanos
+
+latin_america <- coviddata %>% filter(location %in% latin_america_countries) # filtra casos apenas no vetor
+
+mlatin <- latin_america %>% group_by(location) %>% mutate(row = row_number()) %>% select(location, new_cases, row) # cria matriz dos países, agrupando por local, criando uma nova linha com index e selecionando apenas algumas variáveis
+
+# filtra dados para garantir que todos os países tenham mesmo nro de casos
+result <- mlatin %>% group_by(location) %>% filter(row == max(row))
+mlatin <- mlatin %>% filter(row<=min(result$row)) 
+
+# pivota o data frame de long para wide
+mlatinw <- mlatin %>% pivot_wider(names_from = row, values_from = new_cases) %>% remove_rownames %>% column_to_rownames(var="location")
+
+general_data<-fread("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ7EhiAkgUkjnBSJEsXn5qlvxZwwxF1uHdfuBa9X7uwdH9aTihZtnVcid8s1jGHtw/pubhtml?gid=1417881037&single=true") # carrega dados de DATARSU PERNAMBUCO
+
+ 
+mlatin <- latin_america %>% group_by(location) %>% mutate(row = row_number()) %>% select(location, new_cases, row) # cria matriz dos países, agrupando por local, criando uma nova linha com index e selecionando apenas algumas variáveis
+
+# filtra dados para garantir que todos os países tenham mesmo nro de casos
+result <- mlatin %>% group_by(location) %>% filter(row == max(row))
+mlatin <- mlatin %>% filter(row<=min(result$row))
+
+# pivota o data frame de long para wide
+mlatinw <- mlatin %>% pivot_wider(names_from = row, values_from = new_cases) %>% remove_rownames %>% column_to_rownames(var="location")
+
+
+library(data.table)
+library(dplyr)
+library(tidyverse)
+
+general_data<-fread("https://covid.ourworldindata.org/data/owid-covid-data.csv") # carrega dados de covid19 no mundo
+
+latin_america_countries<-c("Argentina", "Brazil", "Bolivia", "Chile", "Colombia", "Costa Rica", "Cuba", "Dominican Republic", "Ecuador", "El Salvador", "Guatemala", "Haiti", "Honduras", "Mexico", "Nicaragua", "Panama", "Paraguay", "Peru", "Uruguay", "Venezuela") # vetor que identifica países latino americanos
+
+latin_america<- general_data %>% filter(location %in% latin_america_countries) # filtra casos apenas no vetor
+
+mlatin <- latin_america %>% group_by(location) %>% mutate(row = row_number()) %>% select(location, new_cases, row) # cria matriz dos países, agrupando por local, criando uma nova linha com index e selecionando apenas algumas variáveis
+
+# filtra dados para garantir que todos os países tenham mesmo nro de casos
+
+result <- mlatin %>% group_by(location) %>% filter(row == max(row))
+mlatin <- mlatin %>% filter(row<=min(result$row)) 
+
+# pivota o data frame de long para wide
+
+mlatinw <- mlatin %>% pivot_wider(names_from = row, values_from = new_cases) %>% remove_rownames %>% column_to_rownames(var="location") 
+
+
+Exercício Países latinos na EUROPA
+
+general_data<-fread("https://covid.ourworldindata.org/data/owid-covid-data.csv") # carrega dados de covid19 no mundo
+
+latin_europe_countries<-c("Portugal", "Spain", "French", "Italy") # vetor que identifica países latinos na Europa
+
+latin_europe<- general_data %>% filter(location %in% latin_europe_countries) # filtra casos apenas no vetor
+
+mlatin <- latin_europe %>% group_by(location) %>% mutate(row = row_number()) %>% select(location, new_cases, row) # cria matriz dos países, agrupando por local, criando uma nova linha com index e selecionando apenas algumas variáveis
+
+# filtra dados para garantir que todos os países tenham mesmo número de casos
+
+result <- mlatin %>% group_by(location) %>% filter(row == max(row))
+
+mlatin <- mlatin %>% filter(row<=min(result$row)) 
+
+# pivota o data frame de long para wide nos países latinos europeus no frame covid no mundo
+
+mlatinw <- mlatin %>% pivot_wider(names_from = row, values_from = new_cases) %>% remove_rownames %>% column_to_rownames(var="location") 
+
+View(mlatin)
