0fc35fff26f9a77df62b47545e0b01143a39e26f
Bruno Câmara <110070619+bruno-bcab@users.noreply.github.com>
2022-10-25 22:51:34 -0300

    Add files via upload

---
+++ Exercício LIMPEZA 2.R
@@ -0,0 +1,50 @@
+library(data.table)
+library(dplyr)
+library(tidyverse)
+library(funModeling) 
+
+general_data <- fread("https://covid.ourworldindata.org/data/owid-covid-data.csv") # carrega dados de covid19 no mundo
+
+latin_america_countries <-c("Argentina", "Brazil", "Bolivia", "Chile", "Colombia", "Costa Rica", "Cuba", "Dominican Republic", "Ecuador", "El Salvador", "Guatemala", "Haiti", "Honduras", "Mexico", "Nicaragua", "Panama", "Paraguay", "Peru", "Uruguay", "Venezuela") # vetor que identifica países latino americanos
+
+latin_america <- general_data %>% filter(location %in% latin_america_countries) # filtra casos apenas no vetor
+
+latin_america <- latin_america %>% select(location, new_cases, new_deaths)
+
+status(latin_america) # estrutura dos dados (missing etc)
+freq(latin_america) # frequência das variáveis fator
+plot_num(latin_america) # exploração das variáveis numéricas
+profiling_num(latin_america) # estatísticas das variáveis numéricas
+
+latin_america %>% filter(new_cases < 0)
+
+latin_america <- latin_america %>% filter(new_cases>=0)
+guides(scale(freq(latin_europe)))
+library(freqdist)
+library(FREQ)
+install.packages("funModeling")
+
+
+Exercício Limpeza
+
+latin_europe <- general_data %>% filter(location %in% latin_europe_countries) # filtra casos apenas no vetor
+
+latin_europe <- general_data %>% filter(location %in% latin_europe_countries) # filtra casos apenas no vetor
+
+latin_europe <- latin_europe %>% select(location, new_cases, new_deaths)
+
+status(latin_america) # estrutura dos dados (missing etc)
+status(latin_europe)
+library(funModeling)
+freq(latin_europe)
+plot_num(latin_europe) # exploração das variáveis numéricas
+profiling_num(latin_europe) # estatísticas das variáveis numéricas
+latin_europe %>% filter(new_cases < 0)
+library(data.table)
+latin_europe %>% filter(new_cases < 0)
+guides(scale(freq(latin_europe)))
+
+status(latin_europe)
+latin_europe <- general_data %>% filter(general_data %in% 'NA')
+
+latin_europe <- general_data %>% remove(general_data %in% 'NA')
