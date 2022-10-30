df245ac8447925c54f2c0f067b5104944be0a3dd
Bruno Câmara <110070619+bruno-bcab@users.noreply.github.com>
2022-10-29 21:03:33 -0300

    Add files via upload

---
+++ exercício exploração de base de dados.R
@@ -0,0 +1,20 @@
+pacman::p_load(funModeling, tidyverse) 
+
+glimpse(iris) # olhada nos dados
+status(iris) # estrutura dos dados (missing etc)
+freq(iris) # frequência das variáveis fator
+plot_num(iris) # exploração das variáveis numéricas
+profiling_num(iris) # estatísticas das variáveis numéricas
+
+plot_num(iris)
+library(funModeling)
+library(tidyverse)
+
+##EXERCÍCIO EXPLORAÇÃO DE BASE DE DADOS
+
+glimpse(DATARSU) # olhada nos dados
+status(DATARSU) # estrutura dos dados (missing etc)
+freq(DATARSU) # frequência das variáveis fator
+plot_num(iris) # exploração das variáveis numéricas
+profiling_num(DATARSU) # estatísticas das variáveis numéricas
+plot(DATARSU)
